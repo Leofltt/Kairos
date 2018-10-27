26e5cd65b7531c7b954865adab4abd76095ce1fe
Leonardo Foletto <lfoletto@berklee.edu>
2018-10-27 11:06:12 -0400

    updated cabal

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import Kairos
 
 main :: IO ()
-main = someFunc
+main = putStrLn "not yet implemented"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,4 @@
+import Kairos
+
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
