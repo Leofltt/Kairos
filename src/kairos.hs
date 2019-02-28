0b0941a6f08e4a78c85f6f1703e500e92ca64056
Leonardo Foletto <lfoletto@berklee.edu>
2019-02-28 14:49:02 -0500

    removed unused files

--- src/kairos.hs
+++ src/kairos.hs
@@ -2,7 +2,7 @@ module Kairos where
 
 import Kairos.Base
 import Kairos.Network
-import Kairos.Pattern
+--import Kairos.Pattern
 import Kairos.Clock
 import Kairos.TimePoint
 import Kairos.Instrument
