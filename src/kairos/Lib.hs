1168cb767b1a17ea109eb6e3070a6392359086cd
Leonardo Foletto <lfoletto@berklee.edu>
2019-05-07 15:46:58 -0400

    added Lib.hs

---
+++ src/kairos/Lib.hs
@@ -0,0 +1,11 @@
+module Kairos.Lib (module K) where
+
+import Prelude
+
+import Kairos.Base as K
+import Kairos.Network as K
+import Kairos.Clock as K
+import Kairos.TimePoint as K
+import Kairos.Instrument as K
+import Kairos.Player as K
+import Kairos.Utilities as K
