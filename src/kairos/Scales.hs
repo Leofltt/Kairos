94d16f180f0c586bf13b1f2f0be17183ea463056
Leonardo Foletto <lfoletto@berklee.edu>
2019-07-31 12:27:57 -0400

    more scales

--- src/kairos/Scales.hs
+++ src/kairos/Scales.hs
@@ -5,6 +5,7 @@ type Scale = [Double]
 withScale :: Double -> Scale -> Scale
 root `withScale` scale = transpose root scale
 
+-- scales source : https://en.wikipedia.org/wiki/List_of_musical_scales_and_modes
 
 ionian :: Scale
 ionian = [0,2,4,5,7,9,11]
@@ -24,3 +25,9 @@ blues :: Scale
 blues = [1,3,5,6,7,10]
 flamenco :: Scale
 flamenco = [0,1,5,6,8,10,11]
+harmMin :: Scale
+harmMin = [0,2,3,5,7,8,11]
+persian :: Scale
+persian = [0,1,4,5,6,8,11]
+prometheus :: Scale
+prometheus = [0,2,4,6,9,10]
