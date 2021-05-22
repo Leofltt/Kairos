adcd2ab21fbe74897f8723964d62649dacb9c457
Leonardo Foletto <flttleonardo@gmail.com>
2021-05-22 22:24:12 +0200

    fixed Markov bug + now can use tables in format [[]] or csv files

--- src/kairos/Markov.hs
+++ src/kairos/Markov.hs
@@ -10,31 +10,40 @@ import Data.Maybe
 import Control.Concurrent.STM
 
 
--- runMarkov csv path -> PfPat -> PfPat
-runMarkovSimple :: String -> [Pfield]->  IO [Pfield]
-runMarkovSimple cs pat = do
+runMarkovSimpleCSV :: String -> [Pfield] ->  IO [Pfield]
+runMarkovSimpleCSV cs pat = do
   doc <- parseCSVFromFile cs
-  let file = prepareCSV doc
-  let note = length $ filter (< ( head pat))  pat
+  let table = fmap stringToDouble $ fmap tail $ prepareCSV doc
+  runMarkovSimple table pat 
+
+runMarkovSimple :: [[Double]] -> [Pfield] ->  IO [Pfield]
+runMarkovSimple table pat = do
+  let note = length $ filter (< ( head pat)) pat
   prob <- randF
-  let list = scanl1 (+) (stringToDouble $ pickRow note file)
-  let newList = listFromIndex (sort pat) $ fromJust $ pickIndex prob  list
+  let list = scanl1 (+) (pickRow note table)
+  let newList = listFromIndex pat $ fromJust $ pickIndex prob list
   return $ newList
 
-
-
-runMarkov :: String -> PfPat -> IO Pfield
-runMarkov file n = do
+runMarkovCSV :: String -> PfPat -> IO Pfield
+runMarkovCSV file n = do
   patrn <- readTVarIO (pat n)
-  pat' <- runMarkovSimple file patrn
+  pat' <- runMarkovSimpleCSV file patrn
+  atomically $ writeTVar (pat n) pat'
+  return $ head pat'
+
+runMarkov :: [[Double]] -> PfPat -> IO Pfield
+runMarkov table n = do
+  patrn <- readTVarIO (pat n)
+  pat' <- runMarkovSimple table patrn
   atomically $ writeTVar (pat n) pat'
   return $ head pat'
 
 
 pickProb4Index :: Double -> [Double] -> Double
 pickProb4Index perc (x:xs) | x >= perc = x
+                           | (x <= perc) && ((head xs > perc)) =  head xs
                            | (x <= perc) && ((head xs) <= perc) = pickProb4Index perc (xs++[x])
-                           | (x <= perc) && ((head xs > perc))=  x
+                           
 
 pickIndex val list = elemIndex (pickProb4Index val list) list
 
@@ -42,13 +51,12 @@ listFromIndex :: [Pfield] -> Int -> [Pfield]
 listFromIndex list indx = firstnote:(filter (/= firstnote) list) where
   firstnote = (!!) list indx
 
-
-
-prepareCSV  a = tail $ noEmptyRows a
+prepareCSV a = tail $ noEmptyRows a
 
 noEmptyRows = either (const []) (filter (\row -> 2 <= length row))
 
-pickRow indx prepFile = tail $ (!!) prepFile indx
+pickRow :: Int -> [[a]] -> [a]
+pickRow indx prepFile = (!!) prepFile indx
 
 removeNewLine list = (init list) ++ [filter removeNL (last list)]
 
