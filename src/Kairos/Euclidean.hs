35f9d9087b4955debb26e6e39f88712e2e9a5e6c
leofltt <flttleonardo@gmail.com>
2025-06-01 18:15:23 +0200

    fixed the euclidean rotation

--- src/Kairos/Euclidean.hs
+++ src/Kairos/Euclidean.hs
@@ -1,40 +1,43 @@
-module Kairos.Euclidean where 
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
--- Taken and adapted from haskell music theory 
+module Kairos.Euclidean where
+
+-- Taken and adapted from haskell music theory
 -- https://hackage.haskell.org/package/hmt
 
-type STEP a = ((Int,Int),([[a]],[[a]]))
+type STEP a = ((Int, Int), ([[a]], [[a]]))
 
 left :: STEP a -> STEP a
-left ((i,j),(xs,ys)) =
-    let (xs',xs'') = splitAt j xs
-    in ((j,i-j),(zipWith (++) xs' ys,xs''))
+left ((i, j), (xs, ys)) =
+  let (xs', xs'') = splitAt j xs
+   in ((j, i - j), (zipWith (++) xs' ys, xs''))
 
 right :: STEP a -> STEP a
-right ((i,j),(xs,ys)) =
-    let (ys',ys'') = splitAt i ys
-    in ((i,j-i),(zipWith (++) xs ys',ys''))
+right ((i, j), (xs, ys)) =
+  let (ys', ys'') = splitAt i ys
+   in ((i, j - i), (zipWith (++) xs ys', ys''))
 
 bjorklund' :: STEP a -> STEP a
-bjorklund' (n,x) =
-    let (i,j) = n
-    in if min i j <= 1
-       then (n,x)
-       else bjorklund' (if i > j then left (n,x) else right (n,x))
+bjorklund' (n, x) =
+  let (i, j) = n
+   in if min i j <= 1
+        then (n, x)
+        else bjorklund' (if i > j then left (n, x) else right (n, x))
 
-bjorklund :: (Int,Int) -> [Int]
-bjorklund (i,j') =
-    let j = j' - i
-        x = replicate i [1]
-        y = replicate j [0]
-        (_,(x',y')) = bjorklund' ((i,j),(x,y))
-    in concat x' ++ concat y'
+bjorklund :: (Int, Int) -> [Int]
+bjorklund (i, j') =
+  let j = j' - i
+      x = replicate i [1]
+      y = replicate j [0]
+      (_, (x', y')) = bjorklund' ((i, j), (x, y))
+   in concat x' ++ concat y'
 
 euclidean :: (Int, Int) -> Int -> [Int]
-euclidean x shift = rotate shift $ bjorklund x  
+euclidean x shift = rotate shift $ bjorklund x
 
--- todo : fix this to work in both directions, 
--- right now it shifts left w/ positive values (counterintuitive)
--- and doesn't really behave with negative values
-rotate ::  Int -> [a] -> [a]
-rotate n list = take (length list) (drop n (cycle list)) 
+-- todo : fix this to work in both directions,
+-- neg vals for left / pos for right
+rotate :: Int -> [a] -> [a]
+rotate n list
+  | n < 0 = take (length list) $ drop (abs n) $ cycle list
+  | n >= 0 = reverse $ take (length list) $ drop n $ cycle $ reverse list
--- src/Kairos/Markov.hs
+++ src/Kairos/Markov.hs
@@ -1,22 +1,23 @@
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
 module Kairos.Markov where
 
-import Kairos.Pfield ( Pfield )
-import Kairos.PfPat ( PfPat(pat) )
-import Kairos.Utilities ( randF, stringToDouble )
-import Text.CSV ( parseCSVFromFile )
-import Data.Char ( isNumber, isSymbol )
-import Data.List ( elemIndex )
-import Data.Maybe ( fromJust )
-import Control.Concurrent.STM ( atomically, readTVarIO, writeTVar )
+import Control.Concurrent.STM (atomically, readTVarIO, writeTVar)
+import Data.Char (isNumber, isSymbol)
+import Data.List (elemIndex)
+import Data.Maybe (fromJust)
+import Kairos.PfPat (PfPat (pat), Updater)
+import Kairos.Pfield (Pfield)
+import Kairos.Utilities (randF, stringToDouble)
+import Text.CSV (parseCSVFromFile)
 
-runMarkovSimpleCSV :: String -> [Pfield] ->  IO [Pfield]
+runMarkovSimpleCSV :: String -> [Pfield] -> IO [Pfield]
 runMarkovSimpleCSV cs patt = do
   doc <- parseCSVFromFile cs
   let table = fmap (stringToDouble . tail) (prepareCSV doc)
   runMarkovSimple table patt
 
-runMarkovSimple :: [[Double]] -> [Pfield] ->  IO [Pfield]
+runMarkovSimple :: [[Double]] -> [Pfield] -> IO [Pfield]
 runMarkovSimple table patt = do
   let note = length $ filter (< head patt) patt
   prob <- randF
@@ -25,7 +26,7 @@ runMarkovSimple table patt = do
   return newList
 
 -- | updater to run Markov on CSV file
-runMarkovCSV :: String -> PfPat -> IO Pfield
+runMarkovCSV :: String -> Updater
 runMarkovCSV file n = do
   patrn <- readTVarIO (pat n)
   pat' <- runMarkovSimpleCSV file patrn
@@ -33,7 +34,7 @@ runMarkovCSV file n = do
   return $ head pat'
 
 -- | updater to run Markov on hand coded transition table [[Double]]
-runMarkov :: [[Double]] -> PfPat -> IO Pfield
+runMarkov :: [[Double]] -> Updater
 runMarkov table n = do
   patrn <- readTVarIO (pat n)
   pat' <- runMarkovSimple table patrn
@@ -41,22 +42,25 @@ runMarkov table n = do
   return $ head pat'
 
 -- | shorthand versions
-rMkv :: [[Double]] -> PfPat -> IO Pfield
+rMkv :: [[Double]] -> Updater
 rMkv = runMarkov
-rMkvCSV :: String -> PfPat -> IO Pfield
+
+rMkvCSV :: String -> Updater
 rMkvCSV = runMarkovCSV
 
 pickProb4Index :: Double -> [Double] -> Double
-pickProb4Index perc (x:xs) | x >= perc = x
-                           | (x <= perc) && (head xs > perc) =  head xs
-                           | (x <= perc) && (head xs <= perc) = pickProb4Index perc (xs++[x])
+pickProb4Index perc (x : xs)
+  | x >= perc = x
+  | (x <= perc) && (head xs > perc) = head xs
+  | (x <= perc) && (head xs <= perc) = pickProb4Index perc (xs ++ [x])
 
 pickIndex :: Double -> [Double] -> Maybe Int
 pickIndex val list = elemIndex (pickProb4Index val list) list
 
 listFromIndex :: [Pfield] -> Int -> [Pfield]
-listFromIndex list indx = firstnote:filter (/= firstnote) list where
-  firstnote = (!!) list indx
+listFromIndex list indx = firstnote : filter (/= firstnote) list
+  where
+    firstnote = (!!) list indx
 
 prepareCSV :: Either a [[b]] -> [[b]]
 prepareCSV a = tail $ noEmptyRows a
