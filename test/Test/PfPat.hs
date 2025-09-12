39c97f8e650b1525a93a60005d57dd41186d5263
leofltt <flttleonardo@gmail.com>
2025-09-12 11:02:24 +0200

    fix tests

--- test/Test/PfPat.hs
+++ test/Test/PfPat.hs
@@ -1,58 +1,58 @@
 module Test.PfPat where
 
-import Test.Hspec ( hspec, describe, it, shouldBe )
+import Control.Concurrent.STM (TVar, newTVarIO)
 import Kairos.PfPat
-import Kairos.Pfield ( Pfield(Pd), new_pfId )
-import Control.Concurrent.STM (newTVarIO, TVar)
+import Kairos.Pfield (Pfield (Pd), newPfId)
+import Test.Hspec (describe, hspec, it, shouldBe)
 
 testPat :: IO (TVar [Pfield])
-testPat = newTVarIO [Pd 1,Pd 2,Pd 3]
+testPat = newTVarIO [Pd 1, Pd 2, Pd 3]
 
 testAPat :: IO (TVar [Pfield])
 testAPat = newTVarIO [Pd 0]
 
 testPfPat :: IO PfPat
 testPfPat = do
-    pfP <- testPat
-    return $ PfPat { pat = pfP, pfId = new_pfId 1 "testId", updater = keep }
+  pfP <- testPat
+  return $ PfPat {pat = pfP, pfId = newPfId 1 "testId", updater = keep}
 
 testPfPatA :: IO PfPat
 testPfPatA = do
-    pfP <- testAPat
-    return $ PfPat { pat = pfP, pfId = new_pfId 1 "testId", updater = keep }
+  pfP <- testAPat
+  return $ PfPat {pat = pfP, pfId = newPfId 1 "testId", updater = keep}
 
 pfPatTest :: IO ()
 pfPatTest = hspec $ do
-    describe "test keep updater" $ do
-        it "should return the head of the pat" $ do
-            testPfP <- testPfPat
-            newP <- keep testPfP
-            newP `shouldBe` Pd 1
-    describe "test nextVal updater" $ do
-        it "should return the next value of the pat" $ do
-            testPfP <- testPfPat
-            newP <- nextVal testPfP >> nextVal testPfP
-            newP `shouldBe` Pd 2
-    describe "test retrograde updater" $ do
-        it "should return the last value of the pat" $ do
-            testPfP <- testPfPat
-            newP <- retrograde testPfP >> retrograde testPfP
-            newP `shouldBe` Pd 3
-    describe "test percentNext updater" $ do
-        it "should return the next value of the pat" $ do
-            testPfP <- testPfPat
-            newP <- percentNext 100 testPfP >> percentNext 100 testPfP
-            newP `shouldBe` Pd 2
-        it "should return the head of the pat" $ do
-            testPfP <- testPfPat
-            newP <- percentNext 0 testPfP >> percentNext 0 testPfP
-            newP `shouldBe` Pd 1
-    describe "test auto updater" $ do
-        it "should keep the same value if pat is 1 element" $ do
-            testPfP <- testPfPatA
-            newP <- a testPfP
-            newP `shouldBe` Pd 0
-        it "should return the next value of the pat if pat is longer than 1 element" $ do
-            testPfP <- testPfPat
-            newP <- a testPfP >> a testPfP
-            newP `shouldBe` Pd 2
+  describe "test keep updater" $ do
+    it "should return the head of the pat" $ do
+      testPfP <- testPfPat
+      newP <- keep testPfP
+      newP `shouldBe` Pd 1
+  describe "test nextVal updater" $ do
+    it "should return the next value of the pat" $ do
+      testPfP <- testPfPat
+      newP <- nextVal testPfP >> nextVal testPfP
+      newP `shouldBe` Pd 2
+  describe "test retrograde updater" $ do
+    it "should return the last value of the pat" $ do
+      testPfP <- testPfPat
+      newP <- retrograde testPfP >> retrograde testPfP
+      newP `shouldBe` Pd 3
+  describe "test percentNext updater" $ do
+    it "should return the next value of the pat" $ do
+      testPfP <- testPfPat
+      newP <- percentNext 100 testPfP >> percentNext 100 testPfP
+      newP `shouldBe` Pd 2
+    it "should return the head of the pat" $ do
+      testPfP <- testPfPat
+      newP <- percentNext 0 testPfP >> percentNext 0 testPfP
+      newP `shouldBe` Pd 1
+  describe "test auto updater" $ do
+    it "should keep the same value if pat is 1 element" $ do
+      testPfP <- testPfPatA
+      newP <- aup testPfP
+      newP `shouldBe` Pd 0
+    it "should return the next value of the pat if pat is longer than 1 element" $ do
+      testPfP <- testPfPat
+      newP <- aup testPfP >> aup testPfP
+      newP `shouldBe` Pd 2
--- test/Test/TimePoint.hs
+++ test/Test/TimePoint.hs
@@ -1,40 +1,41 @@
 module Test.TimePoint where
 
-import Test.Hspec ( hspec, describe, it, shouldBe )
 import Kairos.TimePoint
+import Test.Hspec (describe, hspec, it, shouldBe)
 
 timePointTest :: IO ()
 timePointTest = hspec $ do
-    describe "Conversion from and to TP" $ do
-        it "a list of TimePoints from Doubles" $ do
-            toTP [4,5,6] `shouldBe` [TP 4, TP 5, TP 6]
-        it "a list of Doubles from TimePoints" $ do
-            fromTP [TP 7, TP 8, TP 9] `shouldBe` [7,8,9]
-    describe "Check that sequencing TP Patterns works" $ do
-        it "can calculate the next TP" $ do
-            nextBeat (TP 1) [TP 0, TP 1, TP 2, TP 3] `shouldBe` TP 2
-            nextBeat (TP 3) [TP 0, TP 1, TP 2, TP 3] `shouldBe` TP 0
-    describe "Functions to create a list of TimePoints" $ do
-        it "checking tuplesForBar" $ do
-            tupleForBar 4 3 `shouldBe` [TP 0.3333333333333333, TP 1.6666666666666665, TP 3.0]
-            tupleForBar 4 8 `shouldBe` toTP [0.125, 0.625 .. 3.625]
-        it "checking euclidean rhythm (3,8), no rotation" $ do
-            putStrLn " 1 0 0 1 0 0 1 0"
-            euclid (3,8) 0 4 `shouldBe` toTP [0, 1.5, 3]
-        it "checking euclidean rhythm (3,8), one rotation left" $ do
-            putStrLn " 0 0 1 0 0 1 0 1"
-            euclid (3,8) 1 4 `shouldBe` toTP [1, 2.5, 3.5]
-        it "checking euclidean rhythm (3,8), two rotations left" $ do
-            putStrLn " 0 1 0 0 1 0 1 0"
-            euclid (3,8) 2 4 `shouldBe` toTP [0.5, 2, 3]
-        it "checking patternWithDensity" $ do
-            d <- patternWithDensity 4 16 100
-            d `shouldBe` tupleForBar 4 16
-            d <- patternWithDensity 4 10 100
-            d `shouldBe` tupleForBar 4 10
-        it "checking shine" $ do
-            shine 4 "****" `shouldBe` toTP [0,1,2,3]
-            shine 4 "~*~*" `shouldBe` toTP [1,3]
-            shine 4 "~*~~~*~~" `shouldBe` toTP [0.5,2.5]
-            shine 4 "*~~~~~*~~*~~" `shouldBe` toTP [0,2,3]
-        -- todo : implement tests for other functions
+  describe "Conversion from and to TP" $ do
+    it "a list of TimePoints from Doubles" $ do
+      toTP [4, 5, 6] `shouldBe` [TP 4, TP 5, TP 6]
+    it "a list of Doubles from TimePoints" $ do
+      fromTP [TP 7, TP 8, TP 9] `shouldBe` [7, 8, 9]
+  describe "Check that sequencing TP Patterns works" $ do
+    it "can calculate the next TP" $ do
+      nextBeat (TP 1) [TP 0, TP 1, TP 2, TP 3] `shouldBe` TP 2
+      nextBeat (TP 3) [TP 0, TP 1, TP 2, TP 3] `shouldBe` TP 0
+  describe "Functions to create a list of TimePoints" $ do
+    it "checking tuplesForBar" $ do
+      tupleForBar 4 3 `shouldBe` [TP 0.3333333333333333, TP 1.6666666666666665, TP 3.0]
+      tupleForBar 4 8 `shouldBe` toTP [0.125, 0.625 .. 3.625]
+    it "checking euclidean rhythm (3,8), no rotation" $ do
+      putStrLn " 1 0 0 1 0 0 1 0"
+      euclid (3, 8) 0 4 `shouldBe` toTP [0, 1.5, 3]
+    it "checking euclidean rhythm (3,8), one rotation right" $ do
+      putStrLn " 0 1 0 0 1 0 0 1"
+      euclid (3, 8) 1 4 `shouldBe` toTP [0.5, 2, 3.5]
+    it "checking euclidean rhythm (3,8), two rotations left" $ do
+      putStrLn " 0 1 0 0 1 0 1 0"
+      euclid (3, 8) (-2) 4 `shouldBe` toTP [0.5, 2, 3]
+    it "checking patternWithDensity" $ do
+      d <- patternWithDensity 4 16 100
+      d `shouldBe` tupleForBar 4 16
+      pat <- patternWithDensity 4 10 100
+      pat `shouldBe` tupleForBar 4 10
+    it "checking shine" $ do
+      shine 4 "****" `shouldBe` toTP [0, 1, 2, 3]
+      shine 4 "~*~*" `shouldBe` toTP [1, 3]
+      shine 4 "~*~~~*~~" `shouldBe` toTP [0.5, 2.5]
+      shine 4 "*~~~~~*~~*~~" `shouldBe` toTP [0, 2, 3]
+
+-- todo : implement tests for other functions
